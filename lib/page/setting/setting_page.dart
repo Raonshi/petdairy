b7a21478ffba87b4e0e04cfd97da64d819009b43
raon <sunwonsw95@naver.com>
2022-12-24 14:46:46 +0900

    add sign out logic

--- lib/page/setting/setting_page.dart
+++ lib/page/setting/setting_page.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 import 'package:petdiary/config.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -8,24 +10,36 @@ class SettingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () => context.go('/'),
-          iconSize: 32.0,
-          icon: const Icon(Icons.chevron_left_rounded),
-        ),
-        title: const Text('Add New Pet'),
-      ),
-      body: Column(
-        children: [
-          ListTile(
-            onTap: () {
-              SharedPreferences.getInstance().then((value) => value.remove(LocalStorageKey.petList));
-            },
-            title: Text('데이터 초기화'),
+    return LoaderOverlay(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => context.go('/'),
+            iconSize: 32.0,
+            icon: const Icon(Icons.chevron_left_rounded),
           ),
-        ],
+          title: const Text('Add New Pet'),
+        ),
+        body: Column(
+          children: [
+            ListTile(
+              onTap: () {
+                SharedPreferences.getInstance().then((value) => value.remove(LocalStorageKey.petList));
+              },
+              title: Text('데이터 초기화'),
+            ),
+            ListTile(
+              onTap: () {
+                context.loaderOverlay.show();
+                FirebaseAuth.instance.signOut().then((_) {
+                  context.loaderOverlay.hide();
+                  context.go('/auth');
+                });
+              },
+              title: Text('로그아웃'),
+            ),
+          ],
+        ),
       ),
     );
   }
